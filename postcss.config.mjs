f0c0f8b5bc2e15c3acee5629b9b2cf048015bfd9
Abhinav2005-hub <abhinavsaini995@gmail.com>
2025-09-06 16:25:39 +0530

    add tailwind

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,5 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+const config = {
+  plugins: ["@tailwindcss/postcss"],
+};
 
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+export default config;
