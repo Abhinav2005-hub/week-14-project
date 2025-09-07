5b751ceb760d7073adca274a465f2a5b0f0fc923
Abhinav2005-hub <abhinavsaini995@gmail.com>
2025-09-07 16:32:09 +0530

    introduce backend

---
+++ app/api/user/route.ts
@@ -0,0 +1,19 @@
+import { NextResponse, NextRequest } from "next/server";
+
+export function GET(req: NextRequest) {
+    return NextResponse.json({
+        email: "abhinav@gmail.com",
+        name: "abhinav"
+    })
+}
+
+export async function POST(req: NextRequest) {
+    const body = await req.json();
+    console.log(req.headers.get("authorization"));
+
+    console.log(req.nextUrl.searchParams.get("name"));
+
+    return NextResponse.json({
+        message: "You are signed up"
+    })
+}
--- app/user/page.tsx
+++ app/user/page.tsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 async function fetchData() {
     console.log('111111');
-    const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details");
+    const response = await axios.get("http://localhost:3001/api/user");
     console.log("response is" + JSON.stringify(response.data));
     return response.data;
 }
